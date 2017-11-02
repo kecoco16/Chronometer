1c519e1b22396b763bcb4210e0979832ea31112c
kecoco <kecoco16@gmail.com>
2017-11-02 14:46:58 -0600

    update

--- timer.js
+++ timer.js
@@ -1,19 +1,20 @@
-function cronometro () {
+function cronometro(min) {
   let segundos = 60
-  let minutos = 9
-  let count = setInterval(function() {
-  segundos--
-  if(segundos == 0 && minutos == 0){
-    console.log('se acabo el tiempo')
-    clearInterval(count)
-  }else{
-    if(segundos == -1){
-      minutos--
-      segundos = 60
+  let minutos = min - 1
+  let count = setInterval(function () {
+    segundos--
+    if (segundos == 0 && minutos == 0) {
+      console.log('se acabo el tiempo')
+      clearInterval(count)
     }
+
+    if (segundos == -1) {
+      minutos--
+      segundos = 59
+    }
+
     let updateSeconds = (segundos < 10) ? '0' + segundos.toString() : segundos
     console.log(minutos + ':' + updateSeconds)
-  }
-}, 1000);
+  }, 1000);
 }
-cronometro()
+cronometro(10)
