438da1f882d9e5e00e83d38ad61371363f174dcd
Kevin Castillo Mora <kecoco16@gmail.com>
2018-03-08 10:01:54 -0600

    Update timer.js
    Ejemplo de como usarlo con react

--- timer.js
+++ timer.js
@@ -1,3 +1,4 @@
+// Javascript
 const chronometer = min => printTime(min)
 
 const printTime = initialMin => {
@@ -20,3 +21,32 @@ const printTime = initialMin => {
 const formaterSeconds = sec => sec < 10 ? '0' + sec.toString() : sec
 
 chronometer(10)
+
+// React 
+state = {
+  chrono: '30 : 00',
+  sec: 60,
+  min: 29
+}
+
+componentDidMount () {
+  this.chrono = setInterval(() => {
+    this.setState({
+      sec: this.state.sec - 1
+    })
+    if (this.state.sec === 0 && this.state.min === 0) {
+      clearInterval(this.chrono)
+    }
+    if (this.state.sec === -1) {
+      this.setState({min: this.state.min - 1})
+      this.setState({sec: 59})
+    }
+    this.setState({
+      chrono: `${this.state.min} : ${this.state.sec < 10 ? '0' his.state.sec.toString() : this.state.sec} `
+    })
+  },1000)
+}
+
+componentWillUnmount () {
+  clearInterval(this.chrono)
+}
